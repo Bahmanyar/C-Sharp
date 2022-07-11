a8ebd3271c32a379b36f19620ae7162e6ee89e34
Bahmanyar <bahmanyar.a.m@gmail.com>
2022-07-11 18:03:14 +0430

    up to clip 14

---
+++ CSharp_Sample/Chapter02.Sample01/00_Comments/Comments.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter02.Sample01._00_Comments
+{
+    internal class Comments
+    {
+        #region [- Chapter 2 -]
+        //TopLevel Templates :
+        //Ommit many codes for start in Program.cs >> from c# 9
+
+        //use "var":
+        //instead of declaring DataType >> from c# 3
+
+        //Target type new expression :
+        //[Person p=new()]; >> from c# 9 
+
+        // Variable vs constant:
+        // const srting s="A";
+        // readonly 
+
+        //null for value types :
+        //int? or Nullable<int>
+        //HasValue property for types
+        //built-in nullable in .net6 apps >>   <Nullable>enable</Nullable> in click on the project name
+
+        //nint & uint in c#9
+
+        //put binary or hexadecimal in int  [0b & 0x] >> from c#9
+
+        //separator for numbers [ int x=123_321_432] >> from c#7
+
+        //Half data type >> C#5
+        //Single data type >> C#5
+        //
+        #endregion
+    }
+}
